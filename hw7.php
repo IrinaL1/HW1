3b51bc6f854fbceb2924aa208b6eb4125cf75b22
IrinaL1 <lavrinenko.ia@phystech.edu>
2021-12-12 01:35:17 +0300

    second

--- hw7.php
+++ hw7.php
@@ -3,4 +3,10 @@
 	$lesson = htmlspecialchars($_POST["lesson"]);
 	echo $say;
 	echo $lesson;
+
+	$json = file_get_contents('php://input');
+	$data = json_decode($json);
+	$data->Smth_new = "Где-то";
+	$data = json_encode($data);
+	echo $data;
 ?>
